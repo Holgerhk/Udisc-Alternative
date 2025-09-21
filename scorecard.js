6c746e3fc4403c7f193070795362f4ff672103ac
Holger Krogh <holger.krogh@gmail.com>
2025-09-21 15:53:18 +0200

    added holes to object

--- scorecard.js
+++ scorecard.js
@@ -8,28 +8,54 @@ class Course {
     }
 }
 
-const courses = [
-    {location: "Ryhaven Skanderborg", par: 61, id: "first", holes: [{number: 1, par: 3, length: 105}, {number: 2, par: 3, length: 105}]},
-    {location: "Tilst Park", par: 65, id: "second"}
+const coursesData = [
+    {location: "Ryhaven Skanderborg", 
+    totalPar: 61, 
+    holes: [
+        {number: 1, par: 3, length: 69}, 
+        {number: 2, par: 3, length: 70},
+        {number: 3, par: 3, length: 65},
+        {number: 4, par: 4, length: 134},
+        {number: 5, par: 3, length: 87},
+        {number: 6, par: 5, length: 179},
+        {number: 7, par: 4, length: 127},
+        {number: 8, par: 3, length: 59},
+        {number: 9, par: 3, length: 85},
+        {number: 10, par: 3, length: 76},
+        {number: 11, par: 3, length: 75},
+        {number: 12, par: 3, length: 63},
+        {number: 13, par: 4, length: 119},
+        {number: 14, par: 3, length: 73},
+        {number: 15, par: 3, length: 84},
+        {number: 16, par: 3, length: 58},
+        {number: 17, par: 4, length: 145},
+        {number: 18, par: 4, length: 132}
+    ]},
+    {location: "Tilst Park", 
+    par: 65,
+    holes: [
+        {number: 1, par: 3, length: 73}, 
+        {number: 2, par: 3, length: 84},
+        {number: 3, par: 3, length: 76},
+        {number: 4, par: 4, length: 154},
+        {number: 5, par: 3, length: 47},
+        {number: 6, par: 4, length: 190},
+        {number: 7, par: 3, length: 63},
+        {number: 8, par: 3, length: 74},
+        {number: 9, par: 3, length: 56},
+        {number: 10, par: 3, length: 64},
+        {number: 11, par: 4, length: 103},
+        {number: 12, par: 4, length: 172},
+        {number: 13, par: 3, length: 122},
+        {number: 14, par: 3, length: 65},
+        {number: 15, par: 4, length: 128},
+        {number: 16, par: 3, length: 105},
+        {number: 17, par: 4, length: 147},
+        {number: 18, par: 3, length: 75}
+    ]}
 ];
 
-const holes = [
-    {id: "first", hole1: 3, hole2: 4, hole3: 5},
-    second = {hole1: 4, hole2: 5, hole3: 3}
-];
-
-courses.forEach(data => {
+coursesData.forEach(data => {
     const course = new Course(data.location, data.par);
     course.show();
 });
-
-console.log(holes[1]);
-
-function test(courses, holes) {
-    if (courses[0].id == holes[0].id) {
-        console.log("yes!");
-        
-    }
-}
-test(courses, holes);
-console.log("fsjofodf");
